62a7ef948b40b9225d6b9f8657d48d4e6c90d755
gdi2290 <github@gdi2290.com>
2016-03-06 10:19:26 -0800

    chore: current workaround

--- webpack.config.js
+++ webpack.config.js
@@ -25,46 +25,50 @@ module.exports = {
       { test: /\.ts$/, loader: 'ts-loader' },
     ],
     noParse: [
-      root('node_modules/angular2/bundles/angular2-polyfills'),
-      root('node_modules/angular2/bundles'),
-      root('node_modules/zone.js/dist'),
-      root('node_modules/zone.js/dist/zone-microtask'),
-      root('node_modules/es6-shim'),
-      root('node_modules/es6-promise'),
-      root('node_modules/reflect-metadata')
+      // root('node_modules/angular2/bundles/angular2-polyfills'),
+      // root('node_modules/angular2/bundles'),
+      // root('node_modules/zone.js/dist'),
+      // root('node_modules/zone.js/dist/zone-microtask'),
+      // root('node_modules/es6-shim'),
+      // root('node_modules/es6-promise'),
+      // root('node_modules/reflect-metadata')
     ]
   },
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(true),
     new webpack.optimize.CommonsChunkPlugin({ name: ['no_uglify', 'polyfills'], filename: '[name].bundle.js', minChunks: Infinity }),
     new UglifyJsPlugin({
       exclude: [
-        'no_uglify.bundle.js',
+        // 'no_uglify.bundle.js',
       ],
     //   // warnings: true,
     //   comments: false,
-      // beautify: true,
-      // compress: {
-      //   sequences     : false,  // join consecutive statemets with the “comma operator”
-      //   properties    : false,  // optimize property access: a["foo"] → a.foo
-      //   dead_code     : false,  // discard unreachable code
-      //   drop_debugger : false,  // discard “debugger” statements
-      //   unsafe        : false, // some unsafe optimizations (see below)
-      //   conditionals  : false,  // optimize if-s and conditional expressions
-      //   comparisons   : false,  // optimize comparisons
-      //   evaluate      : false,  // evaluate constant expressions
-      //   booleans      : false,  // optimize boolean expressions
-      //   loops         : false,  // optimize loops
-      //   unused        : false,  // drop unused variables/functions
-      //   hoist_funs    : false,  // hoist function declarations
-      //   hoist_vars    : false, // hoist variable declarations
-      //   if_return     : false,  // optimize if-s followed by return/continue
-      //   join_vars     : false,  // join var declarations
-      //   cascade       : false,  // try to cascade `right` into `left` in sequences
-      //   side_effects  : false,  // drop side-effect-free statements
-      //   global_defs   : {},
-      //   keep_fnames: true
-      // },
+      beautify: true,
+      compress: {
+        sequences     : false,  // join consecutive statemets with the “comma operator”
+        properties    : false,  // optimize property access: a["foo"] → a.foo
+        dead_code     : false,  // discard unreachable code
+        drop_debugger : false,  // discard “debugger” statements
+        unsafe        : false, // some unsafe optimizations (see below)
+        conditionals  : false,  // optimize if-s and conditional expressions
+        comparisons   : false,  // optimize comparisons
+        evaluate      : false,  // evaluate constant expressions
+        booleans      : false,  // optimize boolean expressions
+        loops         : false,  // optimize loops
+        unused        : false,  // drop unused variables/functions
+        hoist_funs    : false,  // hoist function declarations
+        hoist_vars    : false, // hoist variable declarations
+        if_return     : false,  // optimize if-s followed by return/continue
+        join_vars     : false,  // join var declarations
+        cascade       : false,  // try to cascade `right` into `left` in sequences
+        side_effects  : false,  // drop side-effect-free statements
+        global_defs   : {},
+        keep_fnames: true
+      },
       // mangle: false // false to get it to work
+      mangle: {
+        except: ['RouterLink']
+      }
 
     })
   ],
