39eba30ffa0cef98628ccbf16317d26cccf5d415
gdi2290 <github@gdi2290.com>
2016-03-07 22:20:00 -0800

    chore: correct import

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import {provide, bootstrap} from 'angular2/platform/browser';
-import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+import {provide, Component} from 'angular2/core';
 import {
   RouteConfig,
   ROUTER_DIRECTIVES,
